e24e47290a1c07506cbdd1fe93d6a2be0a028381
Lucas Nascimento <lucasjoao85@gmail.com>
2019-01-03 15:35:10 -0200

    Create Array.kt

---
+++ Array.kt
@@ -0,0 +1,15 @@
+/**
+ * Arrays
+ * play.kotlinlang.org 
+ */
+
+fun main(args: Array<String>) {
+    //Criação de Arrays em geral
+    var nomes = arrayOf("Lucas", "Paulo","Jiselle","Vitória")
+    
+    
+    
+    //Array de inteiros
+    var numeros = intArrayOf(11,111,11111)
+    
+}
