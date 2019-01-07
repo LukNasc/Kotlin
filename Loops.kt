eb2c58ae1387a2d3ddbd19c1dbdf54ab2bfe72a0
Lucas Nascimento <lucasjoao85@gmail.com>
2019-01-07 15:46:32 -0200

    Create Loops.kt

---
+++ Loops.kt
@@ -0,0 +1,33 @@
+    /*
+    Loops
+     while
+        for
+    */
+    fun main(args: Array<String>) {
+     /*
+        var numero = 1
+        while( numero < 5 ){
+            //códigos a serem executados
+            println("Executado: $numero")
+            numero++
+        }*/
+        
+        /*
+        for( numero in 1..5 ){
+            println("Executado: $numero")
+        }*/
+        
+        val postagens = arrayOf(
+           "Descoberto novo método de tratamento para Diabetes!", 
+           "Novo curso de Android foi lançado!",
+           "Bolsa de valores opera em alta de 2%.",
+            "Postagem adicionada"
+        )
+        
+        for( (indice, postagem) in postagens.withIndex() ){
+            println( "$indice - $postagem" )
+        }
+        
+    }
+
+
