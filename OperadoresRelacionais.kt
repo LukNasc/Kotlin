4d9bf50765e5cd1da34e2576daeb774af4a650e3
Lucas Nascimento <lucasjoao85@gmail.com>
2019-01-07 12:44:19 -0200

    Create OperadoresRelacionais.kt

---
+++ OperadoresRelacionais.kt
@@ -0,0 +1,18 @@
+    fun main(args: Array<String>) {
+        /*
+        Relacionais
+     == (Igual a)
+     != (Diferente)
+     > (maior que)
+     < (Menor que)
+     >= (Maior ou igual)
+     <= (Menor ou igual)
+     Lógicos
+     && ( e )
+     || ( ou )
+        */
+        var notaProfessor = 5//0-10
+        var notaProvaGeral = 5 //0-10
+        println( notaProfessor > 6 || notaProvaGeral >= 6 )
+        
+    }
