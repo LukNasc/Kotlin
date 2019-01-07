89b22f981efb9ed4a64b3d39dac0a5943ee8b8de
Lucas Nascimento <lucasjoao85@gmail.com>
2019-01-07 12:34:38 -0200

    Create OperadoresBasicos.kt

---
+++ OperadoresBasicos.kt
@@ -0,0 +1,16 @@
+    fun main(args: Array<String>) {
+     
+        /*
+        Somar (+)
+        Subtrair (-)
+        Multiplicar (*)
+        Divisao (/)
+        Módulo (%) - Resto de um divisão
+        */
+        var numero1 = 35
+        var numero2 = 5
+        var resultado = numero1 % numero2
+        
+        println( resultado )
+        
+    }
